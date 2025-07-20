5bd6388fc755f4c99210f547fe0cec4404ffc959
Liuyichen-lyc <liu_yichen@sjtu.edu.cn>
2025-07-20 21:21:49 +0800

    Update __init__.py

--- agents/ai_bots/__init__.py
+++ agents/ai_bots/__init__.py
@@ -7,11 +7,15 @@ from .minimax_bot import MinimaxBot
 from .mcts_bot import MCTSBot
 from .rl_bot import RLBot
 from .behavior_tree_bot import BehaviorTreeBot
+from .snake_ai import SnakeAI, SmartSnakeAI
+
 
 __all__ = [
     'RandomBot',
     'MinimaxBot',
     'MCTSBot',
     'RLBot',
-    'BehaviorTreeBot'
+    'BehaviorTreeBot',
+    'SnakeAI',
+    'SmartSnakeAI'
 ] 
